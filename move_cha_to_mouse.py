458a3fcb666d72b6ca17248a13c2a7e3bf0734ae
2002OID <ykkim308321@gmail.com>
2023-10-01 15:26:35 +0900

    종료키와 마우스 좌표 받기

--- move_cha_to_mouse.py
+++ move_cha_to_mouse.py
@@ -8,10 +8,24 @@ character = load_image('animation_sheet.png')
 hand_arrow = load_image('hand_arrow.png')
 
 x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
+mx, my = TUK_WIDTH // 2, TUK_HEIGHT // 2
 frame = 0
 running = True
 arrow = 1
+
 def handle_events():
+    global running
+    global mx, my
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+        elif event.type == SDL_MOUSEBUTTONDOWN:
+            if event.button == SDL_BUTTON_LEFT:
+                mx, my = event.x, event.y
+
     pass
 
 while running:
